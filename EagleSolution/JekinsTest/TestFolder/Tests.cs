bc1c8ef7176005646e2f7e2dc0e35dd9dc057784
Deji Akin <presideji@yahoo.com>
2019-03-11 06:36:46 +0000

    #Added Selenium Command

--- EagleSolution/JekinsTest/TestFolder/Tests.cs
+++ EagleSolution/JekinsTest/TestFolder/Tests.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace JekinsTest.TestFolder
 {
@@ -10,7 +13,14 @@ namespace JekinsTest.TestFolder
         public void TestMethod1()
         {
             //Console.WriteLine("This is test to see if test runs on the Jenkins environment");
-            Assert.IsTrue(true);
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.next.co.uk/");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Id("sli_search_1")).SendKeys("Hello Lady");
+            Thread.Sleep(10000);
+            var result = driver.Title.Contains("Next");
+            Assert.IsTrue(result);
+            driver.Quit();
         }
 
         [TestMethod]
