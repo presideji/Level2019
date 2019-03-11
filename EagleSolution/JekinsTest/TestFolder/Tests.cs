98ec42b268de02ed7be59e52aef2b6717e4ffe2e
Deji Akin <presideji@yahoo.com>
2019-03-11 05:40:40 +0000

    Modify Steps

---
+++ EagleSolution/JekinsTest/TestFolder/Tests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JekinsTest.TestFolder
+{
+    [TestClass]
+    public class FirtsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+            Console.WriteLine("This is test to see if test runs on the Jenkins environment");
+        }
+    }
+
+}
