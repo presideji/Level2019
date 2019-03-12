086faf694b5f51d9e901aab93875a833c4d5a14f
Deji Akin <presideji@yahoo.com>
2019-03-12 20:13:24 +0000

    Full Framework2

---
+++ JekinsTest/EagleSolutionMyWay/DriverManager/DriverInstance.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace EagleSolutionMyWay.DriverManager
+{
+    public class DriverInstance
+    {
+        protected RemoteWebDriver Driver { get; set; }
+    }
+}
---
+++ JekinsTest/EagleSolutionTest/Hooks/TestHooks.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EagleSolutionMyWay.DriverManager;
+using EagleSolutionTest.BrowserFactory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+
+namespace EagleSolutionTest.Hooks
+{
+    public class TestHooks : DriverInstance
+    {
+        private Browsers browser;
+        //protected IWebDriver Driver;
+        public TestContext testContext { get; set; }
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext testContext)
+        {
+
+
+        }
+
+
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+
+        }
+
+
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //this.browser = new Browsers();
+            Driver = browser.LaunchBrowser();
+
+        }
+
+
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            browser.CloseBrowser();
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
